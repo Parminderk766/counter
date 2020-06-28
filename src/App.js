edb9419ee8e005cd0be936cd7746982b4ea7c94d
Parminderk766 <51168168+Parminderk766@users.noreply.github.com>
2020-06-28 15:08:30 -0400

    Completed Counter Workshop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count:0,
+      isDouble: false,
+      changeIncrement: "Single Increments"
+    };
+  }
+
+  increment = () => {
+    if(this.state.count < 20){
+      
+      if(this.state.isDouble && this.state.count !== 19){
+        this.setState ({
+          count: this.state.count + 2
+        })
+      } else if (!this.state.isDouble) {
+        this.setState ({
+          count: this.state.count + 1
+        })
+      }
+    }
+  }
+
+  decrement = () => {
+
+    if(this.state.count > 0){
+      this.setState ({
+        count: this.state.count - 1
+      })
+    }
+  }
+
+  clear = () => {
+    this.setState ({
+      count: 0
+    })
+  }
+
+  toggle = () => {
+
+    this.setState ({
+      isDouble: !this.state.isDouble
+    })
+
+    if(this.state.isDouble){
+      this.setState ({
+        changeIncrement: "Double Increments"
+      });
+    } else {
+      this.setState ({
+        changeIncrement: "Single Increments"
+      });
+    }
+
+  }
+
+  render () {
+    return(
+      <div className = 'container'>
+        <div className = 'navbar'>Counter.js</div>
+        <div className = 'counter'>
+          <h1>{this.state.count}</h1>
+          <button type = 'button' onClick = {this.increment}>Increment</button>
+          <button type = 'button' onClick = {this.decrement}>Decrement</button>
+          <button type = 'button' onClick = {this.clear}>clear</button>
+          <button type = 'button' onClick = {this.toggle}>{this.state.changeIncrement}</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Counter;
